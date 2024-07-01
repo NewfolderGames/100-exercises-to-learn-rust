cfe8571c196a163fa1ddc11cfb8139206a54fe9c
이민용 <newtolder@koolsign.net>
2024-07-01 13:00:05 +0900

    4-10까지 진행

--- exercises/04_traits/10_assoc_vs_generic/src/lib.rs
+++ exercises/04_traits/10_assoc_vs_generic/src/lib.rs
@@ -13,6 +13,42 @@
 // You don't have to though: it's perfectly okay to write three separate
 // implementations manually. Venture further only if you're curious.
 
+pub trait Power<T> {
+    
+    fn power(&self, p: T) -> u32;
+
+}
+
+impl Power<u16> for u32 {
+    
+    fn power(&self, p: u16) -> u32 {
+        
+        self.pow(p as u32)
+        
+    }
+    
+}
+
+impl Power<u32> for u32 {
+    
+    fn power(&self, p: u32) -> u32 {
+        
+        self.pow(p)
+        
+    }
+    
+}
+
+impl Power<&u32> for u32 {
+    
+    fn power(&self, p: &u32) -> u32 {
+        
+        self.power(*p)
+        
+    }
+    
+}
+
 #[cfg(test)]
 mod tests {
     use super::Power;
